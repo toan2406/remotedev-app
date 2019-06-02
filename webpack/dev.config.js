e43f15570e38f06f46c64fbdccb16f2e99dfce9c
Toan Nguyen <toan2406@gmail.com>
2019-06-03 00:26:34 +0700

    Change config to support yarn link

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -5,47 +5,57 @@ import baseConfig from './base.config';
 const port = 3000;
 const entry = [
   `webpack-dev-server/client?http://localhost:${port}`,
-  'webpack/hot/only-dev-server'
+  'webpack/hot/only-dev-server',
 ];
 
 const config = baseConfig({
+  resolve: { modules: [path.resolve(__dirname, 'node_modules')] },
   input: {
-    app: [path.join(__dirname, '../src/web/index'), ...entry]
+    app: [path.join(__dirname, '../src/web/index'), ...entry],
   },
   output: {
     path: path.join(__dirname, '../dev/js'),
-    publicPath: `http://localhost:${port}/js/`
+    publicPath: `http://localhost:${port}/js/`,
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
-  loaders: [{
-    test: /\.js$/,
-    loader: 'babel',
-    exclude: /node_modules/,
-    query: {
-      presets: ['es2015', 'stage-0', 'react'],
-      plugins: [
-        'add-module-exports',
-        'transform-decorators-legacy',
-        [
-          'react-transform',
-          {
-            transforms: [{
-              transform: 'react-transform-hmr',
-              imports: ['react'],
-              locals: ['module']
-            }, {
-              transform: 'react-transform-catch-errors',
-              imports: ['react', 'redbox-react']
-            }]
-          }
-        ]
-      ]
-    }
-  }],
-  entry
+  loaders: [
+    {
+      test: /\.js$/,
+      loader: 'babel',
+      exclude: /node_modules/,
+      query: {
+        presets: [
+          require.resolve('babel-preset-es2015'),
+          require.resolve('babel-preset-stage-0'),
+          require.resolve('babel-preset-react'),
+        ],
+        plugins: [
+          require.resolve('babel-plugin-add-module-exports'),
+          require.resolve('babel-plugin-transform-decorators-legacy'),
+          [
+            require.resolve('babel-plugin-react-transform'),
+            {
+              transforms: [
+                {
+                  transform: 'react-transform-hmr',
+                  imports: ['react'],
+                  locals: ['module'],
+                },
+                {
+                  transform: 'react-transform-catch-errors',
+                  imports: ['react', 'redbox-react'],
+                },
+              ],
+            },
+          ],
+        ],
+      },
+    },
+  ],
+  entry,
 });
 
 config.devtool = 'eval';
